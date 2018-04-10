2f18ac263b1e328e607c0e84b46eabd334a6a81b
Froyd <gabaritocrypter@gmail.com>
2018-04-10 03:53:02 -0300

    commit

--- app/src/main/java/com/example/froyd/beasy/MainActivity.java
+++ app/src/main/java/com/example/froyd/beasy/MainActivity.java
@@ -85,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
                                     mTextView.setText("Erro no login");
                                 }
 
-
+//
 
 
                             }
