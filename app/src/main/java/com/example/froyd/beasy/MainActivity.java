be2282076349adba6f77cc59018560fc53356eb9
thomazmatos <guerronmatos@gmail.com>
2018-04-10 21:54:14 -0400

    pre req google maps

--- app/src/main/java/com/example/froyd/beasy/MainActivity.java
+++ app/src/main/java/com/example/froyd/beasy/MainActivity.java
@@ -1,28 +1,24 @@
 package com.example.froyd.beasy;
 
-        import android.app.Activity;
         import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.TextView;
-        import android.widget.Toast;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-        import com.android.volley.AuthFailureError;
-        import com.android.volley.Request;
-        import com.android.volley.RequestQueue;
-        import com.android.volley.Response;
-        import com.android.volley.VolleyError;
-        import com.android.volley.toolbox.StringRequest;
-        import com.android.volley.toolbox.Volley;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 //        import org.omg.CORBA.PUBLIC_MEMBER;
-
-        import java.util.HashMap;
-        import java.util.Map;
 
     //    import static sun.text.bidi.BidiBase.R;
 
