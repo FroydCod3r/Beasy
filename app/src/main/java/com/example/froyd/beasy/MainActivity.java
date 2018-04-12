d1b5c977bdf65fa1d9241b9fbe30b4f169088286
thomazmatos <guerronmatos@gmail.com>
2018-04-12 15:44:22 -0400

    permissions 100% workson + change name oficina

--- app/src/main/java/com/example/froyd/beasy/MainActivity.java
+++ app/src/main/java/com/example/froyd/beasy/MainActivity.java
@@ -84,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
 
             final TextView mTextView = (TextView) findViewById(R.id.json_view);
             final RequestQueue queue = Volley.newRequestQueue(this);
-            final Intent intent = new Intent(this, maps.class);
+            final Intent intent = new Intent(this, Oficina.class);
             startActivity(intent);
 
             final EditText login_input = (EditText) findViewById(R.id.login_edit);
@@ -157,7 +157,7 @@ public class MainActivity extends AppCompatActivity {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    final Intent intent = new Intent(this, maps.class);
+                    final Intent intent = new Intent(this, Oficina.class);
                     finish();
                     startActivity(intent);
 
