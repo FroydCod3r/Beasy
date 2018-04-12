32c6bb4d48ca4bd553e4a40d77f5ccb7250d017f
thomazmatos <guerronmatos@gmail.com>
2018-04-12 00:25:14 -0400

    mapa function 100 % workson + permission

--- app/src/main/java/com/example/froyd/beasy/maps.java
+++ app/src/main/java/com/example/froyd/beasy/maps.java
@@ -1,6 +1,8 @@
 package com.example.froyd.beasy;
 
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,42 +23,51 @@ public class maps extends AppCompatActivity implements OnMapReadyCallback {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        Boolean mLocationPermissionGranted = false;
 
-        
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
 
-
-        carregarmapa();
-    }
-
-
-
-
-    public void carregarmapa() {
-
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap map) {
-
-                LatLng oficina1 = new LatLng(-25.443150, -49.238243);
-                map.addMarker(new MarkerOptions().position(oficina1).title("Oficina do jailson"));
-                map.moveCamera(CameraUpdateFactory.newLatLng(oficina1));
-                map.setMinZoomPreference(14.0f);
-                map.setMaxZoomPreference(16.0f);
-
-            }
-        });
+            carregarmapa();
 
 
-
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    1234);
+        }
 
     }
 
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
+        public void carregarmapa () {
 
+            SupportMapFragment mapFragment =
+                    (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+            mapFragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap map) {
+
+                    LatLng oficina1 = new LatLng(-25.443150, -49.238243);
+                    map.addMarker(new MarkerOptions().position(oficina1).title("Oficina do jailson"));
+                    map.moveCamera(CameraUpdateFactory.newLatLng(oficina1));
+                    map.setMinZoomPreference(11.5f);
+                    map.setMaxZoomPreference(14.0f);
+                    map.setMyLocationEnabled(true);
+
+                }
+            });
+
+
+        }
+
+
+        @Override
+        public void onMapReady (GoogleMap googleMap){
+
+        }
     }
-}
+
 
