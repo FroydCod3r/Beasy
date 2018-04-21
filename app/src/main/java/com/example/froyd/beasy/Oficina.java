b4c0cf19a47ce1a0319e3bdaf8148b116cd80c9b
thomazmatos <guerronmatos@gmail.com>
2018-04-20 21:56:12 -0400

    koe ee

--- app/src/main/java/com/example/froyd/beasy/Oficina.java
+++ app/src/main/java/com/example/froyd/beasy/Oficina.java
@@ -1,32 +1,60 @@
 package com.example.froyd.beasy;
 
 import android.annotation.SuppressLint;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class Oficina extends AppCompatActivity implements OnMapReadyCallback {
+    public FusedLocationProviderClient mFusedLocationClient;
 
+
+    public LatLng myloc;
+
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oficina);
 
-        carregarmapa();
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        final LatLng myloc = new LatLng(location.getLatitude(), location.getLongitude());
+                        carregarmapa(myloc);
+
+                        if (location != null) {
+                            // Logic to handle location object
+                        }
+                    }
+                });
+
+
 
     }
 
 
-    public void carregarmapa() {
+
+    public void carregarmapa(final LatLng myloc) {
 
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @SuppressLint("MissingPermission")
             @Override
@@ -34,11 +62,18 @@ public class Oficina extends AppCompatActivity implements OnMapReadyCallback {
 
                 LatLng oficina1 = new LatLng(-25.443150, -49.238243);
                 map.addMarker(new MarkerOptions().position(oficina1).title("Oficina do jailson"));
-                map.moveCamera(CameraUpdateFactory.newLatLng(oficina1));
+                map.moveCamera(CameraUpdateFactory.newLatLng(myloc));
                 map.setMinZoomPreference(11.5f);
                 map.setMaxZoomPreference(14.0f);
 
                 map.setMyLocationEnabled(true);
+
+
+
+
+
+
+
 
 
             }
@@ -48,8 +83,11 @@ public class Oficina extends AppCompatActivity implements OnMapReadyCallback {
     }
 
 
+    @SuppressLint("MissingPermission")
     @Override
-    public void onMapReady (GoogleMap googleMap){
+    public void onMapReady (final GoogleMap googleMap){
 
     }
+
+
 }
