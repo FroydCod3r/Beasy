9e4001d4d60cb2ffb714406cf72982aa7da0c574
thomazmatos <guerronmatos@gmail.com>
2018-04-27 04:57:23 -0400

    oloko

---
+++ app/src/main/java/com/example/froyd/beasy/Oficinaadapter.java
@@ -0,0 +1,17 @@
+package com.example.froyd.beasy;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+public class Oficinaadapter extends ArrayAdapter<Oficinaobj> {
+    private final Context context;
+    private final ArrayList<Oficinaobj> elementos;
+
+
+    public Oficinaadapter(Context context, ArrayList<Oficinaobj> elementos){
+        super(context, R.layout.)
+
+    }
+}
