ae949692b2fab03e3aa70cfcede7b2cb93317d80
thomazmatos <guerronmatos@gmail.com>
2018-04-27 05:11:26 -0400

    list 50% workson

--- app/src/main/java/com/example/froyd/beasy/Oficinaadapter.java
+++ app/src/main/java/com/example/froyd/beasy/Oficinaadapter.java
@@ -1,7 +1,12 @@
 package com.example.froyd.beasy;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -11,7 +16,25 @@ public class Oficinaadapter extends ArrayAdapter<Oficinaobj> {
 
 
     public Oficinaadapter(Context context, ArrayList<Oficinaobj> elementos){
-        super(context, R.layout.)
+        super(context, R.layout.linha, elementos);
+        this.context = context;
+        this.elementos = elementos;
+
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.linha, parent, false);
+
+        TextView nome = (TextView) rowView.findViewById(R.id.nome);
+        TextView descricao = (TextView) rowView.findViewById(R.id.descricao);
+        TextView distancia = (TextView) rowView.findViewById(R.id.distancia);
+        TextView horario = (TextView) rowView.findViewById(R.id.horario);
+        ImageView imagem = (ImageView) rowView.findViewById(R.id.imagem);
+        ImageView imgavaliacao = (ImageView) rowView.findViewById(R.id.imgavaliacao);
+        return  rowView;
+
+
 
     }
 }
